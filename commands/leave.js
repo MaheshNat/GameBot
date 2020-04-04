d42626f093ea565febe16b9b6890c339ceeb06d9
Mahesh Natamai <maheshkumar.natamai@gmail.com>
2020-04-04 18:20:21 -0500

    Fixed more issues with leave command

--- commands/leave.js
+++ commands/leave.js
@@ -12,12 +12,18 @@ module.exports = {
       return message.reply(
         'You have to be playing a game to use this command.'
       );
+    if (game.createdBy === message.author.username) {
+      message.reply(
+        `Sucessfully ended a game of ${game.name}. Since you are the creator, this game will be discontinued.`
+      );
+      return games.splice(game);
+    }
     message.reply(
       `Sucessfully left a game of ${game.name} hosted by ${game.createdBy}.`
     );
     let index = games.indexOf(game);
-    game.players.splice(message.author.username);
-    if (game.players.length === 0) games.splice(game);
+    game.players.splice(message.author.username, 1);
+    if (game.players.length === 0) games.splice(index, index);
     else games[index] = game;
   },
 };
